ff6be7d65e2906c52ccc19d1abefe30de6745c13
SerhiiChynin <sergey.chinin@gmail.com>
2023-03-14 17:35:47 +0200

    style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 function Main() {
     return (
-        <div className="main_elem navbar-dark bg-dark">
+        <div className="main_elem navbar-dark bg-dark ">
             <div>
-                <NavLink className="nav-link" to="/create">Create Note </NavLink>
+                <NavLink className="nav-link link-bottom" to="/create">Create Note </NavLink>
             </div>
             <div>
                 <NavLink className="nav-link" to="/note">Note </NavLink>
