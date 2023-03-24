2e01d6be8b33d014ba70b022124bd14f3dac910e
SerhiiChynin <sergey.chinin@gmail.com>
2023-03-24 21:10:24 +0200

    add-style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,23 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
-        <div className="header, navbar-dark, bg-dark">
-            <h1 className="liteNote">Lite Note</h1>
-            <nav className="nav">
-                <ul>
-                    <li><NavLink  className="nav-link " to="/">Home</NavLink> </li>
-                    <li><NavLink className="nav-link" to="/note">Note</NavLink> </li>
-                    <li><NavLink className="nav-link" to="/create">Create</NavLink> </li>
-                    <li><NavLink className="nav-link" to="/about">About</NavLink> </li>
+        <div className="header,  bg-dark">
+        <h1 className="liteNote">Lite Note</h1>
+            <nav className=" navbar bg-dark navbar-expand-lg " >
+                <div className="container">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                           data-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggle-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navContent">
+                <ul className="navbar-nav mb-lg-0">
+                    <li className=""><NavLink className="nav-link " to="/">Home</NavLink> </li>
+                    <li className=""><NavLink className="nav-link " to="/note">Note</NavLink> </li>
+                    <li className=""><NavLink className="nav-link " to="/create">Create</NavLink> </li>
+                    <li className=""><NavLink className="nav-link " to="/about">About</NavLink> </li>
                 </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     );
