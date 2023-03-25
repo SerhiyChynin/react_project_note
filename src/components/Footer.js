43386abb93bd4502b7c0a51770c8a32e99e23de8
SerhiiChynin <sergey.chinin@gmail.com>
2023-03-25 15:30:40 +0200

    adaptive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
     return (
         <div className="footer bg-dark">
-           <div className="h6  bg-dark">  2023,  Lite Note </div>
-           <div className="h6  bg-dark">  All Rights Reserved</div>
+           <div className="h6  bg-dark">  2023,  Lite Note</div>
+           <div className="h6  bg-dark"> All Rights Reserved </div>
         </div>
     );
 }
