ce50f73560186b5d8a71f5f1ebc289398abc289d
SerhiiChynin <sergey.chinin@gmail.com>
2023-03-27 14:18:38 +0300

    nice app

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,21 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <div className="header,  bg-dark ">
-        <h1 className="liteNote">Lite Note</h1>
-            <nav className=" navbar bg-dark navbar-expand-lg " >
+         <h1 className="liteNote"><NavLink className="nav-link " to="/">LiteNote</NavLink></h1>
+            <nav className=" navbar bg-dark navbar-expand-lg  " >
+
+                <div className="container">
                     <button className="navbar-toggler" type="button" data-toggle="collapse"
-                           data-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
+                           data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                <div className="container">
-                    <div className="collapse navbar-collapse" id="navContent">
-                <ul className="navbar-nav mb-lg-0">
-                    <li className=""><NavLink className="nav-link " to="/">Home</NavLink> </li>
-                    <li className=""><NavLink className="nav-link " to="/note">Note</NavLink> </li>
-                    <li className=""><NavLink className="nav-link " to="/create">Create</NavLink> </li>
-                    <li className=""><NavLink className="nav-link " to="/about">About</NavLink> </li>
-                </ul>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav mb-lg-0">
+                            <li className="nav-item"><NavLink className="nav-link " to="/">Home</NavLink> </li>
+                            <li className="nav-item"><NavLink className="nav-link " to="/create">Create</NavLink> </li>
+                            <li className="nav-item"><NavLink className="nav-link " to="/note">Note</NavLink> </li>
+                            <li className="nav-item"><NavLink className="nav-link " to="/about">About</NavLink> </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,30 @@
 import { NavLink } from "react-router-dom";
 function Main() {
     return (
+        <div className="flex">
         <div className=" flex main_elem navbar-dark bg-dark ">
-            <div>
+            <div className="note">
                 <NavLink className="nav-link link-bottom" to="/create">Create Note </NavLink>
             </div>
             <div className="note">
                 <NavLink className="nav-link  " to="/note">Note </NavLink>
-
             </div>
-        </div>
+            
+            </div>
+               <div className="text-1">
+                    <p><b>LiteNote</b> – сервис для обмена заметками. Создайте заметку, отправьте ссылку на заметку и ваш друг сможет ее просмотреть.
+                После просмотра заметка будет удалена (или по истечении 15 минут с момента создания).</p>
+                <p>Как сделать заметку? </p>
+                <div>
+                    <ul>
+                        <li>Пройдите по ссылке</li>
+                        <li>Вставьте текст и нажмите Create</li>
+                        <li>Отправьте сгенерированный адрес другу!</li>
+                    </ul>
+                </div>
+                    <p>Как прочитать заметку? Перейдите по присланному URL, либо введите адрес руками здесь.</p>
+                </div>
+            </div>
     );
 }
 
